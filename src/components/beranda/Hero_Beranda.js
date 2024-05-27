38e085f5754d4a471484b75b7dff7e9b89998183
johanpramudito <ajohanpramudito@gmail.com>
2024-05-27 10:30:06 +0700

    feat : beranda

---
+++ src/components/beranda/Hero_Beranda.js
@@ -0,0 +1,52 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Hero_Beranda() {
+    const [stretchedIndex, setStretchedIndex] = useState(0);
+
+    const images = [
+        "/img/home/Foto1.png",
+        "/img/home/Foto2.png",
+        "/img/home/Foto3.png",
+    ];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+        setStretchedIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 3000); // Ganti gambar setiap 3 detik
+
+        return () => clearInterval(interval);
+    }, [images.length]);
+
+    const handleImageClick = (index) => {
+        setStretchedIndex(index);
+    };
+
+    const defaultWidths = ["w-[8.854vw]", "w-[11.458vw]", "w-[8.854vw]"];
+    const stretchedWidth = ["w-[31.25vw]", "w-[33.854vw]", "w-[31.25vw]"];
+    const hoverWidths = ["hover:w-[31.25vw]", "hover:w-[33.854vw]", "hover:w-[31.25vw]"];
+    return (
+        <div className="h-screen flex flex-row justify-center items-center bg-white">
+            <div className="w-[30%] mr-[7.24vw] gap-y-[2.708vw] flex flex-col">
+            <h1 className="text-[1.823vw] text-black"><span className=" font-archivo  ">KMTETI merupakan singkatan dari </span><span className=" font-archivosemibold">Keluarga Mahasiswa Teknik Elektro dan Teknologi Informasi.</span></h1>
+            <h2 className="text-[1.302vw] text-black"><span className=" font-archivothin">KMTETI adalah organisasi resmi yang ada di Departemen Teknik Elektro dan Teknologi Informasi (DTETI). Salah satu semboyan yang dimiliki KMTETI adalah </span><span className=" font-archivosemibolditalic">Profesionalisme Berbasis Kekeluargaan.</span></h2>
+            </div>
+            <div className="w-[53.646vw] h-[30.729vw] flex gap-x-[1.042vw]">
+                {images.map((src, index) => (
+                    <div
+                    key={index}
+                    className={`h-[30.729vw] duration-700 ease-in-out transition-[width] rounded-[1.146vw] ${
+                        stretchedIndex === index ? stretchedWidth : `${defaultWidths[index]} ${hoverWidths[index]}`
+                    }`}
+                    onClick={() => handleImageClick(index)}
+                    >
+                    <img
+                        className="object-cover w-full h-full rounded-[1.146vw] transition"
+                        src={src}
+                        alt={`Foto ${index + 1}`}
+                    />
+                    </div>
+                ))}
+        </div>
+        <img className="absolute bottom-[3vw]" src="/img/kepengurusan/arrow.png" alt="" />
+      </div>
+    )
+}
---
+++ src/components/beranda/Intro_Beranda.js
@@ -0,0 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Intro_Beranda() {
+    return (
+        <div className="w-full h-screen flex flex-row justify-center items-center bg-white">
+        <div className="relative w-full h-screen flex flex-col justify-center items-center">
+          <img src="/img/home/Rectangle.png" className="z-[0] absolute object-cover h-screen w-screen" alt="" />
+          <div className="bg-black w-full absolute h-screen opacity-70"></div>
+          <div className="z-[9]">
+            <iframe className="z-[9999] w-[66.563vw] h-[37.396vw]" src="https://www.youtube.com/embed/PGAydha01RI?si=8uSzuCJU27CGtcdX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          <h2 className="z-[9] text-[3.646vw] font-redditsanssemibold text-white mt-[2vw]">Tagline Kita Sekarang</h2>
+          <hr className="z-[9] w-[46.12vw] h-[0.2vw] bg-white"></hr>
+          <h3 className="z-[9] text-[1.823vw] font-redditsanslight text-white">Gala Nawasena KMTETI FT UGM 2024</h3>
+        </div>
+      </div>
+    )
+}
