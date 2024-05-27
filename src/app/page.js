c4a4ae794fcaee6a5e15f2062cbff6fe0cac39c1
johanpramudito <ajohanpramudito@gmail.com>
2024-05-27 19:52:16 +0700

    feat : add Footer layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
-"use client";
 import Hero_Beranda from "@/components/beranda/Hero_Beranda";
 import Intro_Beranda from "@/components/beranda/Intro_Beranda";
 import Update_Beranda from "@/components/beranda/Update_Beranda";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+
 
 export default function Beranda() {
-  
   return (
     <main className="flex flex-col items-center justify-center">
       <Hero_Beranda />
