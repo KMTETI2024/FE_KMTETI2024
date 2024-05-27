13f99b05e06c753e30166feda061bd6f6321a6be
johanpramudito <ajohanpramudito@gmail.com>
2024-05-27 22:09:50 +0700

    feat : template BPO and PH

---
+++ src/app/(pages)/kepengurusan/PH/page.js
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+export default function BPO() {
+    return (
+        <main className="px-[17.396vw] py-[12.292vw] flex flex-col items-center justify-center bg-white">
+            <h1 className="text-black text-[2.344vw] font-redditsanssemibold">Pengurus Harian KMTETI FT UGM</h1>
+            <h1 className="text-black mb-[3.75vw] text-[1.823vw] font-redditsanssemibold">Periode 2023/2024</h1>
+            <img className="mb-[2.604vw] w-full h-full" src="/img/kepengurusan/BPO/BPO.png" alt="" />
+            <p className=" font-archivo text-[1.302vw] text-[#1E1E1E] text-justify whitespace-pre-line leading-[1.6vw]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in nisl eu turpis faucibus porttitor eu nec enim. Proin ornare vitae lacus ac placerat. Fusce odio est, efficitur non tristique sit amet, hendrerit id purus. Sed a tortor lectus. Donec ut dolor maximus, efficitur nulla quis, iaculis eros. Praesent posuere sed velit eget dapibus. Ut et metus varius augue suscipit venenatis a quis diam. Suspendisse sit amet magna tempor, vehicula turpis eget, porttitor urna. Nulla congue eget nisi nec sodales. Pellentesque pretium magna hendrerit nibh rhoncus iaculis. Morbi diam lectus, laoreet eget hendrerit ac, pretium quis leo.
+            </p>
+            <br></br>
+            <p className="font-archivo text-[1.302vw] text-[#1E1E1E] text-justify whitespace-pre-line leading-[1.6vw]">
+            Fusce quam sem, dignissim sed venenatis at, vulputate sed lorem. Nam lacus leo, tincidunt a sollicitudin ac, sodales id velit. Vivamus efficitur diam non tincidunt auctor. Etiam tincidunt, nibh vel auctor fringilla, sem metus semper lorem, a varius diam justo eget dolor. Praesent semper, sem vitae congue lobortis, massa metus pretium purus, ut consectetur augue velit quis dui. Nullam tempus est ac elit pellentesque, eu pulvinar erat gravida. In ipsum ex, interdum sed libero vitae, accumsan accumsan tortor.</p>
+        </main>
+        )
+    }
